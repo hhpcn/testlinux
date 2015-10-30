171406b4ce86907f306eaee69007fceac36a1d03
Administrator <Administrator@59.77.132.94>
2015-10-30 22:03:40 +0800

    Test

---
+++ testlinuxgit/src/testlinuxgit/Test.java
@@ -0,0 +1,11 @@
+package testlinuxgit;
+
+public class Test {
+public static void main(String[] args) {
+//<<<<<<< HEAD
+	System.err.println("kkkk12");
+//=======
+	System.err.println("kdssdsdsdds");
+   //>>>>>>> refs/remotes/origin/master
+}
+}
